e41280c1a8602e3da4181798dfca685b1361e0d2
JingchaoPeng <71299954+JingchaoPeng@users.noreply.github.com>
2020-11-17 00:33:41 +0800

    Update test_cipher_jp4100.py

--- tests/test_cipher_jp4100.py
+++ tests/test_cipher_jp4100.py
@@ -1,5 +1,13 @@
 from cipher_jp4100 import __version__
 from cipher_jp4100 import cipher_jp4100
+import pytest
 
 def test_version():
     assert __version__ == '0.1.0'
+    
+def test_cipher_1():
+    text = 'test'
+    shift = 3
+    new_text = 'whvw'
+    result = cipher(text, shift)
+    assert result == new_text
